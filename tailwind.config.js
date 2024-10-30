7f40cd707952b6c6c72af355f1dd830160302680
imazlin <mazlin.ismael@gmail.com>
2024-10-30 03:38:08 +0100

    first section size responsive

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
