16614a2d86ea274f26ebdc8467f352cc62fc2ebc
imazlin <mazlin.ismael@gmail.com>
2024-10-30 02:55:33 +0100

    header prototype responsive

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var nav = document.querySelector("nav")
+var x3Bars = document.getElementById("bars-3-icon")
+var xIcon = document.getElementById("x-icon")
+
+
+x3Bars.onclick = () => nav.classList.toggle("hidden")
+xIcon.onclick = () => nav.classList.toggle("hidden")
---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["index.html", "./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
