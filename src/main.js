cbebee41599f8111331b15e77d7fda1702030c9e
imazlin <mazlin.ismael@gmail.com>
2024-10-31 02:02:06 +0100

    responsive section with icons setting up

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 var nav = document.querySelector("nav")
 var x3Bars = document.getElementById("bars-3-icon")
 var xIcon = document.getElementById("x-icon")
-var skillsSection = document.getElementById("skills-section")
 
 x3Bars.onclick = () => nav.classList.toggle("hidden")
 xIcon.onclick = () => nav.classList.toggle("hidden")
