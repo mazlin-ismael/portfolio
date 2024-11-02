1f758ec4cc7ce634238b0ec182dd6f0436dcf5de
imazlin <mazlin.ismael@gmail.com>
2024-11-02 02:20:55 +0100

    card project prototype starting

---
+++ src/swiper.js
@@ -0,0 +1,9 @@
+
+// Initialiser Swiper une fois que le DOM est chargé
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    // loop: "infinite"
+  });
+});
